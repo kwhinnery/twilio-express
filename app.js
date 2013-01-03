618642364ca7144018c9fbf2768a157bdcb17f13
Kevin Whinnery <kevin.whinnery@gmail.com>
2013-01-03 09:09:09 -0600

    adding twilio request validation logic

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.configure('development', function () {
 });
 
 app.get('/', routes.index);
-app.post('/', routes.index);
+app.post('/twiml', routes.twiml);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
-/*
- * GET home page.
- */
+var twilio = require('twilio');
+
+//Get Twilio config
+var config = {};
+
+//prod/heroku
+if (process.env.ACCOUNT_SID) {
+    config.accountSid = process.env.ACCOUNT_SID;
+    config.authToken = process.env.AUTH_TOKEN;
+}
+
+//local dev
+else {
+    config = require('config');
+}
 
 exports.index = function (req, res) {
     res.render('index', { title:'Express' });
+};
+
+exports.twiml = function(req, res) {
+    if (twilio.validateExpressRequest(req)) {
+        var resp = new twilio.TwimlResponse();
+        resp.say('express sez - hello twilio!');
+
+        res.type('text/xml');
+        res.send(resp.toString());
+    }
+    else {
+        res.send('you are not twilio.  Buzz off.');
+    }
 };
