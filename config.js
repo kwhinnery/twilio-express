906c959762691d92d54a7c9a1f917c4ba451aa38
assafhp <assafshp@zahav.net.il>
2016-08-23 16:22:46 +0300

    added config

---
+++ config.js
@@ -0,0 +1,7 @@
+//Configuration for running tests and examples against the Twilio API
+module.exports = {
+    accountSid:'ACc8fa19df0dbf89e18e4b6ea7be15d7dd',
+    authToken:'8d841bcd20a9d43265b386edd25c2ee0',
+    from:'+16512223333', //The Twilio number you've bought or configured
+    to:'+16513334444' //The number you would like to send messages to for testing
+};
